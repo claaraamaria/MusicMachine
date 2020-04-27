743d9d0062ac46a91372f4ccc0ede4c29c9c582f
claaraa <clrstancu@gmail.com>
2020-04-27 22:17:38 +0300

    Bounded Services so that the music can lay even when the activity is paused

--- app/src/main/java/com/claramaria/android/samples/musicmachine/MainActivity.java
+++ app/src/main/java/com/claramaria/android/samples/musicmachine/MainActivity.java
@@ -2,8 +2,12 @@ package com.claramaria.android.samples.musicmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +17,26 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "song";
+    private boolean mBound = false;
+    private PlayerService mPlayerService;
     private Button mDownloadButton;
+    private Button mPlayButton;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            mBound = true;
+            PlayerService.LocalBinder localBinder = (PlayerService.LocalBinder) binder;
+            mPlayerService = localBinder.getService();
+            if ( mPlayerService.isPlaying()) {
+                mPlayButton.setText("Pause");
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDownloadButton = findViewById(R.id.downloadButton);
+        mPlayButton = findViewById(R.id.playButton);
 
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +59,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBound) {
+                    if (mPlayerService.isPlaying()) {
+                        mPlayerService.pause();
+                        mPlayButton.setText("Play");
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, PlayerService.class);
+                        startService(intent);
+                        mPlayerService.play();
+                        mPlayButton.setText("Pause");
+                    }
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
 }
