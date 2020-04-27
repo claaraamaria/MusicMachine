7b3bbe5d036431daea8cbb6499e871f3188910f8
claaraa <clrstancu@gmail.com>
2020-04-27 20:30:43 +0300

    Created Intent Services

---
+++ app/src/main/java/com/claramaria/android/samples/musicmachine/DownloadIntentService.java
@@ -0,0 +1,34 @@
+package com.claramaria.android.samples.musicmachine;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DownloadIntentService extends IntentService {
+    private static final String TAG = DownloadIntentService.class.getSimpleName();
+
+    public DownloadIntentService(){
+        super("DownloadIntentService");
+        setIntentRedelivery(true);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        String song = intent.getStringExtra(MainActivity.KEY_SONG);
+        downloadSong(song);
+    }
+
+    private void downloadSong(String song) {
+        long endTime = System.currentTimeMillis() + 10 * 1000;
+        while (System.currentTimeMillis() < endTime) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, song + "Song downloaded");
+    }
+}
